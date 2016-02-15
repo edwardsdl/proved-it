572bbe24a81cd06367ffb18fcbfca29ad4956217
Dallas Edwards <github@dallasedwards.com>
2016-02-15 16:32:59 -0500

    Added FabricHelper class

---
+++ Proved It!/FabricHelper.swift
@@ -0,0 +1,28 @@
+//
+//  FabricHelper.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 2/15/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+import AWSCognito
+import Crashlytics
+import DigitsKit
+import Fabric
+
+final class FabricHelper {
+    static func initializeFabric() {
+
+        initializeDigits()
+        setIncludedKits()
+    }
+
+    private static func initializeDigits() {
+        
+    }
+
+    private static func setIncludedKits() {
+        Fabric.with([AWSCognito.self, Crashlytics.self, Digits.self])
+    }
+}
