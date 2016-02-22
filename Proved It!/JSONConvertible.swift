c6e4c40fa9810afdf24ee3dab110708e44b6991b
Dallas Edwards <github@dallasedwards.com>
2016-02-22 10:33:47 -0500

    Added several model protocols

---
+++ Proved It!/JSONConvertible.swift
@@ -0,0 +1,14 @@
+//
+//  JSONConvertible.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 2/21/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+import SwiftyJSON
+
+protocol JSONConvertible {
+    init(withJSON json: JSON)
+    func toDictionary() -> Dictionary<String, AnyObject>
+}
---
+++ Proved It!/Validatable.swift
@@ -0,0 +1,11 @@
+//
+//  Validatable.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 2/21/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+protocol Validatable {
+    func isValid() -> Bool
+}
