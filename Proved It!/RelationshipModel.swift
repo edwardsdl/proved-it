1d1c526e55674b7d1ba14274766668858e5aab92
Dallas Edwards <github@dallasedwards.com>
2016-02-22 10:35:06 -0500

    Added several models

---
+++ Proved It!/RelationshipModel.swift
@@ -0,0 +1,13 @@
+//
+//  RelationshipModel.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 2/21/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+import CoreData
+
+final class RelationshipModel {
+
+}
