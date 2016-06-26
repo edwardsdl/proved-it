3b12c3fe11b8900a58b192a806c74484cc166b72
edwardsdl <github@dallasedwards.com>
2016-06-26 02:19:20 -0400

    Updated entity attribute properties

--- Proved It!/Result+CoreDataProperties.swift
+++ Proved It!/Result+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Result+CoreDataProperties.swift
 //  Proved It!
 //
-//  Created by Dallas Edwards on 5/4/16.
+//  Created by Dallas Edwards on 6/26/16.
 //  Copyright © 2016 Angry Squirrel Software. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
