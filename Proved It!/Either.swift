e3a541957ced4b082ab088304642013ff0fbbf6e
edwardsdl <github@dallasedwards.com>
2016-06-26 02:02:26 -0400

    Added Either enumeration

---
+++ Proved It!/Either.swift
@@ -0,0 +1,12 @@
+//
+//  Either.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 6/26/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+enum Either<T, U> {
+    case Left(T)
+    case Right(U)
+}
