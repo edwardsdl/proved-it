cd98939bb120782133060fa3351124e3d2cfa84b
edwardsdl <github@dallasedwards.com>
2016-08-09 12:34:36 -0400

    Added loading view and view controller

---
+++ Proved It!/LoadingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingViewController.swift
+//  Proved It!
+//
+//  Created by Dallas Edwards on 8/8/16.
+//  Copyright © 2016 Angry Squirrel Software. All rights reserved.
+//
+
+import UIKit
+
+final class LoadingViewController: BaseViewController<LoadingView> {
+
+}
